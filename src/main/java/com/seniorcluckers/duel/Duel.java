ca52b16f34c2c2031bdea8d8a064fa43b2c9d584
Alex <chrisalexdlc@hotmail.com>
2022-07-05 23:55:18 -0700

    Pilot.

---
+++ src/main/java/com/seniorcluckers/duel/Duel.java
@@ -0,0 +1,17 @@
+package com.seniorcluckers.duel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Duel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
